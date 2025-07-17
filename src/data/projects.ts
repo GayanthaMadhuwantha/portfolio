27a5a9f811679f9f2211a94196b46f7a0e7579ef
Gayantha Maduwantha <134595232+GayanthaMadhuwantha@users.noreply.github.com>
2025-07-18 00:55:13 +0530

    Refactor project screenshots and add new Online Shoe Store entry

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,22 +12,22 @@ export const projects: Project[] = [
     github: "https://github.com/KavishkaGimhanaDharmasiri/Inventory-and-Distributing-Management-System",
     screenshots: [
       'http://i.postimg.cc/260f2QGC/Lotus.png',
-'http://i.postimg.cc/JnrLN6bT/Login-1.png',
-'http://i.postimg.cc/XY544yjg/Option-1.png',
-'http://i.postimg.cc/qBmkLTD2/Add-Customer-1.png',
-'http://i.postimg.cc/xdKfyNBd/Add-Customer-2.png',
-'http://i.postimg.cc/jqvjFN2w/Add-Route.png',
-'http://i.postimg.cc/DZn2CyKP/Balance-Settlement.png',
-'http://i.postimg.cc/cJNZH46G/Change-Password-1.png',
-'http://i.postimg.cc/7YfqtWjd/Detailed-Order-Receipt.png',
-'http://i.postimg.cc/Hk8snh2X/Distribute-produccts-2.png',
-'http://i.postimg.cc/cJMW8y8g/Email-Verification-1.png',
-'http://i.postimg.cc/tTk98QqQ/Item-Returns.png',
-'http://i.postimg.cc/kGhqXQRT/Report-2.png',
-'http://i.postimg.cc/GtpbC7tB/Report-3.png',
-'http://i.postimg.cc/65gtkhX3/Sales-Orders-1.png',
-'http://i.postimg.cc/2865R6LH/Summery.png',
-'http://i.postimg.cc/DyMw2Tq7/Update-User-Details-1.png',
+      'http://i.postimg.cc/JnrLN6bT/Login-1.png',
+      'http://i.postimg.cc/XY544yjg/Option-1.png',
+      'http://i.postimg.cc/qBmkLTD2/Add-Customer-1.png',
+      'http://i.postimg.cc/xdKfyNBd/Add-Customer-2.png',
+      'http://i.postimg.cc/jqvjFN2w/Add-Route.png',
+      'http://i.postimg.cc/DZn2CyKP/Balance-Settlement.png',
+      'http://i.postimg.cc/cJNZH46G/Change-Password-1.png',
+      'http://i.postimg.cc/7YfqtWjd/Detailed-Order-Receipt.png',
+      'http://i.postimg.cc/Hk8snh2X/Distribute-produccts-2.png',
+      'http://i.postimg.cc/cJMW8y8g/Email-Verification-1.png',
+      'http://i.postimg.cc/tTk98QqQ/Item-Returns.png',
+      'http://i.postimg.cc/kGhqXQRT/Report-2.png',
+      'http://i.postimg.cc/GtpbC7tB/Report-3.png',
+      'http://i.postimg.cc/65gtkhX3/Sales-Orders-1.png',
+      'http://i.postimg.cc/2865R6LH/Summery.png',
+      'http://i.postimg.cc/DyMw2Tq7/Update-User-Details-1.png',
 
     ]
   },
@@ -40,36 +40,36 @@ export const projects: Project[] = [
     technologies: ["React Native", "Firebase", "Chart.js", "Tailwind CSS", "SQLite"],
     link: "https://example.com/pos-app",
     github: "https://github.com/GayanthaMadhuwantha/Point-of-sale-App-Using-React-native",
-    screenshots:[
-    'https://i.postimg.cc/qRx1rCLN/Screenshot-20250331-110744.jpg',
-    'https://i.postimg.cc/VkP0qH1c/Screenshot-20250331-110837.jpg',
-    'https://i.postimg.cc/qRx1rCLN/Screenshot-20250331-110744.jpg',
-'https://i.postimg.cc/PNGXdFh4/Screenshot-20250331-110529.jpg',
-'https://i.postimg.cc/NMzGzjzZ/Screenshot-20250331-110652.jpg',
-'https://i.postimg.cc/L5g4KMd5/Screenshot-20250331-110714.jpg',
-'https://i.postimg.cc/VkP0qH1c/Screenshot-20250331-110837.jpg',
-'https://i.postimg.cc/7PzbWgqV/Screenshot-20250331-111737.jpg',
-'https://i.postimg.cc/hPbvSczQ/Screenshot-20250331-121413.jpg',
-'https://i.postimg.cc/FsQ157RQ/Screenshot-20250331-121422.jpg',
-'https://i.postimg.cc/XYjJqmdH/Screenshot-20250331-121447.jpg',
-'https://i.postimg.cc/fR8b9Cvw/Screenshot-20250331-121454.jpg',
-'https://i.postimg.cc/ZqgKfqg2/Screenshot-20250331-122220.jpg',
-'https://i.postimg.cc/J7j4qMQY/Screenshot-20250331-122320.jpg',
-'https://i.postimg.cc/NfYGCSJs/Screenshot-20250331-122346.jpg',
-'https://i.postimg.cc/wTVqC2Zh/Screenshot-20250331-122350.jpg',
-'https://i.postimg.cc/kggJq8PL/Screenshot-20250331-122407.jpg',
-'https://i.postimg.cc/rwZ8mm8s/Screenshot-20250331-122430.jpg',
-'https://i.postimg.cc/hGHcL5Qq/Screenshot-20250331-123210.jpg',
-'https://i.postimg.cc/GpBbsC2Q/Screenshot-20250331-124157.jpg',
-'https://i.postimg.cc/BbzgbHwN/Screenshot-20250331-104855.jpg',
-'https://i.postimg.cc/L6qD7QnW/Screenshot-20250331-105010.jpg',
-'https://i.postimg.cc/zD2pxYXW/Screenshot-20250331-105627.jpg',
-'https://i.postimg.cc/HsfzC0Mk/Screenshot-20250331-105750.jpg',
-'https://i.postimg.cc/RZKRC31Q/Screenshot-20250331-110512.jpg',
-'https://i.postimg.cc/GttKbqzH/Screenshot-2025-0331-003723.jpg',
+    screenshots: [
+      'https://i.postimg.cc/qRx1rCLN/Screenshot-20250331-110744.jpg',
+      'https://i.postimg.cc/VkP0qH1c/Screenshot-20250331-110837.jpg',
+      'https://i.postimg.cc/qRx1rCLN/Screenshot-20250331-110744.jpg',
+      'https://i.postimg.cc/PNGXdFh4/Screenshot-20250331-110529.jpg',
+      'https://i.postimg.cc/NMzGzjzZ/Screenshot-20250331-110652.jpg',
+      'https://i.postimg.cc/L5g4KMd5/Screenshot-20250331-110714.jpg',
+      'https://i.postimg.cc/VkP0qH1c/Screenshot-20250331-110837.jpg',
+      'https://i.postimg.cc/7PzbWgqV/Screenshot-20250331-111737.jpg',
+      'https://i.postimg.cc/hPbvSczQ/Screenshot-20250331-121413.jpg',
+      'https://i.postimg.cc/FsQ157RQ/Screenshot-20250331-121422.jpg',
+      'https://i.postimg.cc/XYjJqmdH/Screenshot-20250331-121447.jpg',
+      'https://i.postimg.cc/fR8b9Cvw/Screenshot-20250331-121454.jpg',
+      'https://i.postimg.cc/ZqgKfqg2/Screenshot-20250331-122220.jpg',
+      'https://i.postimg.cc/J7j4qMQY/Screenshot-20250331-122320.jpg',
+      'https://i.postimg.cc/NfYGCSJs/Screenshot-20250331-122346.jpg',
+      'https://i.postimg.cc/wTVqC2Zh/Screenshot-20250331-122350.jpg',
+      'https://i.postimg.cc/kggJq8PL/Screenshot-20250331-122407.jpg',
+      'https://i.postimg.cc/rwZ8mm8s/Screenshot-20250331-122430.jpg',
+      'https://i.postimg.cc/hGHcL5Qq/Screenshot-20250331-123210.jpg',
+      'https://i.postimg.cc/GpBbsC2Q/Screenshot-20250331-124157.jpg',
+      'https://i.postimg.cc/BbzgbHwN/Screenshot-20250331-104855.jpg',
+      'https://i.postimg.cc/L6qD7QnW/Screenshot-20250331-105010.jpg',
+      'https://i.postimg.cc/zD2pxYXW/Screenshot-20250331-105627.jpg',
+      'https://i.postimg.cc/HsfzC0Mk/Screenshot-20250331-105750.jpg',
+      'https://i.postimg.cc/RZKRC31Q/Screenshot-20250331-110512.jpg',
+      'https://i.postimg.cc/GttKbqzH/Screenshot-2025-0331-003723.jpg',
 
-    
-  ]
+
+    ]
   },
   {
     id: 3,
@@ -80,14 +80,29 @@ export const projects: Project[] = [
     technologies: ["React", "Tailwind CSS", "Framer Motion"],
     link: "https://gayanthamadhuwantha.vercel.app",
     github: "https://github.com/GayanthaMadhuwantha/portfolio",
-    screenshots:['https://i.postimg.cc/zDxmrf8c/Professional-Portfolio.png',
-'https://i.postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
-'https://i.postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
+    screenshots: ['https://i.postimg.cc/zDxmrf8c/Professional-Portfolio.png',
+      'https://i.postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
+      'https://i.postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
     ]
 
   },
-  {
+
+   {
     id: 4,
+    title: "Online shoe store",
+    description: "Simple and Affordable way to buy diseird shoes from Online Shoe Store(Demo Version).",
+    image: "https://i.postimg.cc/jdZjxpyp/Sole-Style-Premium-Shoe-Store-1.png",
+    category: "Web Development",
+    technologies: ["React", "Tailwind CSS", "Express.js", "MySQL", "Whatsapp api"],
+    link: "https://my-fullstack-vite-app-b7cqfefwcxezfyde.canadacentral-01.azurewebsites.net/",
+    github: "https://github.com/GayanthaMadhuwantha/online-shoe-shop",
+    screenshots: [
+
+      'https://i.postimg.cc/KzBzm45f/Sole-Style-Premium-Shoe-Store.png'
+    ]
+  },
+  {
+    id: 5,
     title: "Betting System",
     description: "Web application for managing betting activities with user authentication and real-time updates.",
     image: "https://i.postimg.cc/mZYM6jw8/React-App.png",
@@ -95,22 +110,22 @@ export const projects: Project[] = [
     technologies: ["Spring Boot", "Java", "React", "RestAPI"],
     link: "https://example.com/betting-app",
     github: "https://github.com/GayanthaMadhuwantha/Betting-System",
-    screenshots:[
+    screenshots: [
       'https://i.postimg.cc/L8dCrxxN/React-App.png',
       'https://i.postimg.cc/h4530DTF/React-App-1.png',
       'https://i.postimg.cc/fTSq2GfV/React-App-2.png',
       'https://postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
       'https://postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
-'https://postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
-'https://postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
-'https://postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
-'https://postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
-'https://postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
+      'https://postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
+      'https://postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
+      'https://postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
+      'https://postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
+      'https://postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
 
     ]
   },
   {
-    id: 5,
+    id: 6,
     title: "Point Of Sale Desktop Application",
     description: "Desktop application mainly for Billing and tracking detailed sales analysis in Restaurant and inventory with real-time analytics reports.",
     image: "https://i.postimg.cc/RFb5LKYm/mockuper-1.png",
@@ -118,64 +133,51 @@ export const projects: Project[] = [
     technologies: ["Electron.js", "Javascript", "MySQL", "Bootstrap"],
     link: "https://example.com/pos-desktop-app",
     github: "https://github.com/GayanthaMadhuwantha/POS-system-using-Electron",
-    screenshots:[
+    screenshots: [
       'https://i.postimg.cc/3NJFrn8P/Screenshot.png',
-'https://i.postimg.cc/76NNypk3/Screenshot-1.png',
-'https://i.postimg.cc/vBGWX3Vc/Screenshot-10.png',
-'https://i.postimg.cc/rmg5Hx1g/Screenshot-11.png',
-'https://i.postimg.cc/Vv6XPv4w/Screenshot-12.png',
-'https://i.postimg.cc/7hW0H4Q2/Screenshot-13.png',
-'https://i.postimg.cc/Kcqnr7v8/Screenshot-14.png',
-'https://i.postimg.cc/QM35x0S0/Screenshot-15.png',
-'https://i.postimg.cc/wTnLn6NK/Screenshot-16.png',
-'https://i.postimg.cc/7Z70kp15/Screenshot-17.png',
-'https://i.postimg.cc/d1F9pJzQ/Screenshot-2.png',
-'https://i.postimg.cc/MGVD9n3r/Screenshot-3.png',
-'https://i.postimg.cc/x8xyXMqq/Screenshot-4.png',
-'https://i.postimg.cc/28y7PcXW/Screenshot-5.png',
-'https://i.postimg.cc/7YRnD3pW/Screenshot-6.png',
-'https://i.postimg.cc/jj06nsq6/Screenshot-7.png',
-'https://i.postimg.cc/P58zqLHr/Screenshot-8.png',
-'https://i.postimg.cc/Dzrrqtm9/Screenshot-9.png',
+      'https://i.postimg.cc/76NNypk3/Screenshot-1.png',
+      'https://i.postimg.cc/vBGWX3Vc/Screenshot-10.png',
+      'https://i.postimg.cc/rmg5Hx1g/Screenshot-11.png',
+      'https://i.postimg.cc/Vv6XPv4w/Screenshot-12.png',
+      'https://i.postimg.cc/7hW0H4Q2/Screenshot-13.png',
+      'https://i.postimg.cc/Kcqnr7v8/Screenshot-14.png',
+      'https://i.postimg.cc/QM35x0S0/Screenshot-15.png',
+      'https://i.postimg.cc/wTnLn6NK/Screenshot-16.png',
+      'https://i.postimg.cc/7Z70kp15/Screenshot-17.png',
+      'https://i.postimg.cc/d1F9pJzQ/Screenshot-2.png',
+      'https://i.postimg.cc/MGVD9n3r/Screenshot-3.png',
+      'https://i.postimg.cc/x8xyXMqq/Screenshot-4.png',
+      'https://i.postimg.cc/28y7PcXW/Screenshot-5.png',
+      'https://i.postimg.cc/7YRnD3pW/Screenshot-6.png',
+      'https://i.postimg.cc/jj06nsq6/Screenshot-7.png',
+      'https://i.postimg.cc/P58zqLHr/Screenshot-8.png',
+      'https://i.postimg.cc/Dzrrqtm9/Screenshot-9.png',
 
     ]
   },
   {
-    id: 6,
+    id: 7,
     title: "Weather App",
     description: "Mobile application for tracking weather conditions and forecasts.",
     image: "https://i.postimg.cc/Xv1Qtc6D/Screenshot-2024-0904-110137-portrait.png",
     category: "Mobile App",
     technologies: ["React Native", "Tailwind CSS", "Visualcrossing Weather API"],
     link: "https://example.com/weather-app",
-    github: "https://github.com/GayanthaMadhuwantha/Weather-App", 
-     screenshots:[
+    github: "https://github.com/GayanthaMadhuwantha/Weather-App",
+    screenshots: [
 
       'https://i.postimg.cc/MHb87ZVC/Screenshot-2024-0904-110137.jpg',
       'https://i.postimg.cc/y6FB4jtL/Screenshot-2024-0904-110207.jpg',
       'https://i.postimg.cc/ZK4mT3h4/Screenshot-20240909-140932.jpg',
 
 
-'https://i.postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
-'https://i.postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
-'https://i.postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
-'https://i.postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
-     ]
+      'https://i.postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
+      'https://i.postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
+      'https://i.postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
+      'https://i.postimg.cc/3xq0JY2N/Screenshot-2024-0904-110223.jpg',
+    ]
   },
-{
-  id: 7,
-    title: "Online shoe store",
-    description: "Simple and Affordable way to buy diseird shoes from Online Shoe Store(Demo Version).",
-    image: "https://i.postimg.cc/jdZjxpyp/Sole-Style-Premium-Shoe-Store-1.png",
-    category: "Web Development",
-    technologies: ["React", "Tailwind CSS", "Express.js","MySQL","Whatsapp api"],
-    link: "https://my-fullstack-vite-app-b7cqfefwcxezfyde.canadacentral-01.azurewebsites.net/",
-    github: "https://github.com/GayanthaMadhuwantha/online-shoe-shop", 
-     screenshots:[
-
-  'https://i.postimg.cc/KzBzm45f/Sole-Style-Premium-Shoe-Store.png'
-     ]
-}
+ 
 
 
 ];
